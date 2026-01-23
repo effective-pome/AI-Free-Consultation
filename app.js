7e9eaf45342412f62cce63a70066c9bd07936ec4
effective-pome <katestagelash2026@gmail.com>
2026-01-23 19:51:12 +0900

    Update GAS web app URL in app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const AppState = {
 // GAS連携設定
 // ========================================
 // Google Apps ScriptのウェブアプリURL（デプロイ後に設定してください）
-const GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycby0LWn-ekZYOW-M0QTtWLBZeErpe0h0m2CyPYIX8OwWR0cG_MkgBVqzKJCcDhDMgsjANA/exec';
+const GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwzUMIf6fzW3H34zJT14IERA5bOw9Yg-2Bu71WXaQlDL98oHpaZyep-BcQw0X1vz9lPIA/exec';
 
 // ========================================
 // メール送信設定
